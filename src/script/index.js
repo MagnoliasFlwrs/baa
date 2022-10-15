225ee40b403d41f8c259b8c31951b9cc760f9e6f
Savenok Marta <magnolia.sav@gmail.com>
2022-10-16 00:21:16 +0300

    update menu

--- src/script/index.js
+++ src/script/index.js
@@ -4,6 +4,9 @@ const scroll = document.querySelector('.scroll__to__top');
 const dropdown = document.querySelector('.dropdown')
 const submenu = document.querySelector('.submenu')
 const mobileBack = document.querySelector('.mobile__back__link')
+const linksToSecond = document.querySelectorAll('.link__to__second')
+const secondMenu = document.querySelector('.second__menu')
+const secondMobileBack = document.querySelectorAll('.mobile__back__link__second')
 
 if (burgerMenu) {
     burgerMenu.addEventListener('click' , (e) => {
@@ -28,15 +31,30 @@ function scrollButtonVisibility() {
 document.addEventListener('scroll' , (e) => {
     scrollButtonVisibility();
 })
+function clearClasses(e) {
+    if (e.target.classList.contains('mobile__back__link')) {
+        e.preventDefault();
+		e.target.closest('.burger__submenu').classList.remove('__transform');
+		e.target.closest('.burger__wrap').querySelector('.burger__links').classList.remove('__transform');
+    }
+}
 burgerWrap.addEventListener('click', (e) => {
 	if (e.target.classList.contains('burger__link__dropdown')) {
 		e.preventDefault();
 		e.target.closest('.burger__links').classList.add('__transform');
 		e.target.closest('.burger__link__item').querySelector('.burger__submenu').classList.add('__transform');
 	}
-    if (e.target.classList.contains('mobile__back__link')) {
-        e.preventDefault();
-		e.target.closest('.burger__submenu').classList.remove('__transform');
-		e.target.closest('.burger__wrap').querySelector('.burger__links').classList.remove('__transform');
-    }
+    clearClasses(e)
 });
+linksToSecond.forEach((link) => {
+    link.addEventListener('click' ,(e) => {
+        clearClasses(e)
+        e.target.closest('.burger__submenu__item').querySelector('.second__menu').classList.add('__transformation');
+    })
+})
+secondMobileBack.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.target.closest('.burger__submenu__item').querySelector('.second__menu').classList.remove('__transformation');
+        e.target.closest('.burger__link__item').querySelector('.burger__submenu').classList.add('__transform');
+    })
+})
